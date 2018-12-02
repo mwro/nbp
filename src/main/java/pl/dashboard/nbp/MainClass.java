2f1e11c5c2cc9f99bbac48fbf8b3267a1fd1a016
Marcin <mwro91@gmail.com>
2018-12-02 15:34:07 +0100

    -extracted methods -show connection response message if it's available (i.e 404 message)

--- src/main/java/pl/dashboard/nbp/MainClass.java
+++ src/main/java/pl/dashboard/nbp/MainClass.java
@@ -4,27 +4,61 @@ import com.google.gson.Gson;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class MainClass {
+
     public static void main(String[] args) {
+        connectAndProcessResponse();
+    }
+
+    private static void connectAndProcessResponse() {
+        String arg = "2014-07-04";
+        String url = "http://api.nbp.pl/api/exchangerates/tables/C/" + arg + "?format=json";
+
+        HttpURLConnection openConnection;
         try {
-            InputStream response = new URL("http://api.nbp.pl/api/exchangerates/tables/C/2013-07-04?format=json").openStream();
-
-            try (Scanner scanner = new Scanner(response)) {
-                String responseBody = scanner.useDelimiter("\\A").next();
-                Gson gson = new Gson();
-                ExchangeRatesTable[] exchangeRatesTables = gson.fromJson(responseBody, ExchangeRatesTable[].class);
-
-                List<String> currencyCodes = getCurrencyCodes();
-
-                System.out.print(exchangeRatesTables[0].toString(currencyCodes));
-            }
+            openConnection = openHttpURLConnection(url);
         } catch (IOException e) {
             e.printStackTrace();
+            return;
+        }
+
+        try {
+            processResponse(openConnection);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void processResponse(HttpURLConnection openConnection) throws IOException {
+        InputStream response;
+        try {
+            response = openConnection.getInputStream();
+        } catch (IOException e) {
+            System.out.print(openConnection.getResponseMessage());
+            return;
+        }
+        processResponse(response);
+    }
+
+    private static HttpURLConnection openHttpURLConnection(String url) throws IOException {
+        return (HttpURLConnection) new URL(url).openConnection();
+    }
+
+    private static void processResponse(InputStream response) {
+        try (Scanner scanner = new Scanner(response)) {
+            String responseBody = scanner.useDelimiter("\\A").next();
+            Gson gson = new Gson();
+            ExchangeRatesTable[] exchangeRatesTables = gson.fromJson(responseBody, ExchangeRatesTable[].class);
+
+            List<String> currencyCodes = getCurrencyCodes();
+
+            System.out.print(exchangeRatesTables[0].toString(currencyCodes));
         }
     }
 
